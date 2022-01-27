43695e9ea82a5b4b8fc6d52fa92da3e281b01da0
crr0tz <30696867+crr0tz-4-d1nn3r@users.noreply.github.com>
2022-01-27 15:34:17 -0600

    Add files via upload

--- bob_signs.py
+++ bob_signs.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 27 11:57:05 2022
-
-@author: cruffo
 """
+
 import requests
 from bs4 import BeautifulSoup
 import csv 
 
-url = 'https://bobs-burgers.fandom.com/wiki/Store_Next_Door'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+urls = ['https://bobs-burgers.fandom.com/wiki/Store_Next_Door',
+        'https://bobs-burgers.fandom.com/wiki/Pest_Control_Truck']
 
-sayings = soup.find_all('b')
-
-rows = []
-for item in sayings:
-    rows.append([item.text])
+sayings = []
+for url in urls:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    temp = soup.find_all('b')    
+    
+    for item in temp:
+        sayings.append([item.text])
+    
     
 with open('bob_signs.csv', 'w', newline='', encoding='utf-8') as f:
     write = csv.writer(f)
-    write.writerows(rows) 
+    write.writerows(sayings) 
